4b202188de6b192b1842685845131ee4bf6f6fdd
Christian Bick <christian.bick@bitsuppliers.com>
2019-06-03 16:14:49 +0200

    Also use eslint for test files

--- test/e2e/startup.js
+++ test/e2e/startup.js
@@ -1,7 +1,7 @@
 import { run } from '../../main/server'
 
 describe('Server lifecycle', () => {
-  it ('should start and stop server', async () => {
+  it('should start and stop server', async () => {
     const app = await run(3001)
     await app.stop()
   })
