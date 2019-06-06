fab9597a7baabf48134089469aed49c3a8626091
Christian Bick <christian.bick@bitsuppliers.com>
2019-06-06 13:02:43 +0200

    Fix linting problem

--- test/e2e/ping/health.js
+++ test/e2e/ping/health.js
@@ -2,7 +2,6 @@ import { SERVER_ADDRESS } from '../test-server'
 import request from 'co-request'
 
 describe('Health check', () => {
-
   it('should return 200 on root', async () => {
     const resp = await request(`${SERVER_ADDRESS}/`)
     expect(resp.statusCode).to.equal(200)
