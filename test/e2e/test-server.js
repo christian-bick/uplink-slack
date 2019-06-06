7511f6a8553ac0cd346040b55b2f90990b44928d
Christian Bick <christian.bick@bitsuppliers.com>
2019-06-06 12:58:24 +0200

    Clarify whats happening here

--- test/e2e/test-server.js
+++ test/e2e/test-server.js
@@ -5,19 +5,20 @@ const SERVER_HOST = 'localhost'
 const SERVER_PORT = 3001
 const SERVER_ADDRESS = `${SERVER_PROTOCOL}://${SERVER_HOST}:${SERVER_PORT}`
 
-let app
-
-before(async () => {
-  app = await run(3001)
-})
-
-after(async () => {
-  await app.stop()
-})
-
 export {
   SERVER_PROTOCOL,
   SERVER_HOST,
   SERVER_PORT,
   SERVER_ADDRESS
 }
+
+// The following will start a test server before all tests are run and stop it when all tests have finished
+let app
+
+before(async () => {
+  app = await run(SERVER_PORT)
+})
+
+after(async () => {
+  await app.stop()
+})
