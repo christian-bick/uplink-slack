bc214c2d191e927ba05497e0e3f0f38c3dff8e9e
Christian Bick <christian.bick@bitsuppliers.com>
2019-06-16 21:39:03 +0200

    Keep chats alive and healthy

---
+++ src/chats/chat-health.js
@@ -0,0 +1,37 @@
+import store from '../store'
+
+export const buildJoinUserInfo = (joinedUserId, userProfile, contactProfile) => (
+  `Hi <@${joinedUserId}>! This group is linked to an external conversation with ${contactProfile.name}. ` +
+  `You can follow their conversation here but I will only forward messages of ${userProfile.name}.`
+)
+
+export const rejoinLinkedGroup = (app) => async ({ event, context }) => {
+  const channelId = event.channel
+  const slackGroup = await store.slackGroup.get(channelId)
+  if (!slackGroup) {
+    return
+  }
+  const slackUser = await store.slackUser.get(slackGroup.source.userId)
+  app.client.conversations.invite({
+    token: slackUser.userToken,
+    channel: channelId,
+    users: context.botId
+  })
+}
+
+export const informJoinedUser = (app) => async ({ event, context }) => {
+  console.log(event)
+  const channelId = event.channel
+  const slackGroup = await store.slackGroup.get(channelId)
+  if (!slackGroup) {
+    return
+  }
+  const userProfile = await store.slackProfile.get(slackGroup.source.userId)
+  const contactRegistration = await store.user.registration.get(slackGroup.sink.email)
+  const contactProfile = await store.slackProfile.get(contactRegistration.userId)
+  app.client.chat.postMessage({
+    token: context.botToken,
+    channel: channelId,
+    text: buildJoinUserInfo(event.user, userProfile, contactProfile)
+  })
+}
