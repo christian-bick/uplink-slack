2cbea2c59dae219bc1993493f154442c232f1483
Christian Bick <christian.bick@bitsuppliers.com>
2019-06-16 16:31:13 +0200

    Remove console.log

--- src/chats/chat.js
+++ src/chats/chat.js
@@ -29,7 +29,6 @@ export const forwardMessage = (app) => async ({ context, message }) => {
   const contactTeam = await store.slackTeam.get(contactSlackGroup.source.teamId)
   forwardLog.debug('Forwarding message', { channelId: reverseLink.channelId })
   const userProfile = await store.slackProfile.get(context.userId)
-  console.log(userProfile)
   await app.client.chat.postMessage({
     username: userProfile.name || userProfile.email,
     token: contactTeam.botToken,
