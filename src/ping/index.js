0a5f3106d3b9e56f39fccb76166b88ae44c84958
Christian Bick <christian.bick@bitsuppliers.com>
2019-06-26 15:20:40 +0200

    Cleaning up

--- src/ping/index.js
+++ src/ping/index.js
@@ -1,7 +1,5 @@
-import { replyToGreetings } from './greetings'
 import { respondToHealthCheck } from './health'
 
 export default (app) => {
   app.receiver.app.get('/', respondToHealthCheck)
-  // app.message('hi', replyToGreetings)
 }
