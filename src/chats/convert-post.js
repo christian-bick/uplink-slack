521d4641825306a2f0b08fce32e6bc01b3b61abc
Christian Bick <christian.bick@bitsuppliers.com>
2019-06-27 11:54:51 +0200

    Support formats

--- src/chats/convert-post.js
+++ src/chats/convert-post.js
@@ -1,22 +1,57 @@
 import json2md from 'json2md'
 
+const BLOCK = '```'
+const STRIKE = '~~'
+const ITALIC = '*'
+const BOLD = '**'
+
 json2md.converters.ul = (input) => input.map(item => `- ${item}`).join('\n')
 json2md.converters.ol = (input) => input.map((item, index) => `${index + 1}. ${item}`).join('\n')
-json2md.converters.cl = (input) => input.map(item => `${item}`).join('\n')
+json2md.converters.cl = (input) => input.join('\n')
+json2md.converters.pre = (input) => `${BLOCK}\n${input}\n${BLOCK}`
 
 const MERGE_TYPES = ['ul', 'ol', 'cl']
 
-const preMappers = {
-  cl: ({ type, text, checked }) => ({ type, text: `[${checked ? 'x' : ''}] ${text}` })
+const typeMappers = {
+  cl: ({ type, text, checked }) => `[${checked ? 'x' : ''}] ${text}`
+}
+
+export const formatText = (range, text, formatter) => {
+  const start = range[0]
+  const end = range[1]
+  const head = text.substring(0, start)
+  const body = text.substring(start, end)
+  const tail = text.substring(end, text.length)
+  return `${head}${formatter}${body}${formatter}${tail}`
+}
+
+export const formatMappers = {
+  code: (range, text) => formatText(range, text, BLOCK),
+  strike: (range, text) => formatText(range, text, STRIKE),
+  i: (range, text) => formatText(range, text, ITALIC),
+  b: (range, text) => formatText(range, text, BOLD),
+  u: (range, text) => formatText(range, text, BOLD)
+}
+
+export const applyFormats = (params) => {
+  const formats = params.formats
+  return Object.keys(formats).reduce((result, formatType) => {
+    return formatMappers[formatType] ? formatMappers[formatType](formats[formatType], result) : result
+  }, params.text)
 }
 
 export const convertJson = originalJson => originalJson
   .filter(({ type }) => {
     return !!json2md.converters[type]
   })
-  .map((params) => preMappers[params.type] ? preMappers[params.type](params) : params )
+  .map((params) => {
+    return params.formats ? { ...params, text: applyFormats(params) } : params
+  })
+  .map((params) => {
+    return typeMappers[params.type] ? { ...params, text: typeMappers[params.type](params) } : params
+  })
   .reduce((result, { type, text }, index) => {
-    const prev = index > 0 ? result[result.length - 1] : 0
+    const prev = index > 0 ? result[result.length - 1] : null
     if (MERGE_TYPES.includes(type)) {
       if (prev && Object.keys(prev).includes(type)) {
         prev[type].push(text)
--- src/chats/convert-post.spec.js
+++ src/chats/convert-post.spec.js
@@ -1,10 +1,35 @@
-import {convertPost} from "./convert-post"
+import {applyFormats, convertPost} from './convert-post'
 
 const testInput = '{"version":116,"revision":279,"root":{"children":[{"type":"h1","text":"This is H1"},{"type":"h2","text":"This is H2"},{"type":"p","text":"Paragraph 1"},{"type":"p","text":"Paragraph 2"},{"type":"ul","text":"list item 1"},{"type":"ul","text":"list item 2"},{"type":"ol","text":"numbered item 1"},{"type":"ol","text":"numbered item 2"},{"type":"p","text":"A link","links":{"https://www.google.com":[0,6]}},{"type":"p","text":"Strike Through","formats":{"strike":[0,14]}},{"type":"p","text":"Bold","formats":{"b":[0,4]}},{"type":"p","text":"Underlined","formats":{"u":[0,10]}},{"type":"p","text":"Italic","formats":{"i":[0,6]}},{"type":"p","text":"Code Block","formats":{"code":[0,10]}},{"type":"pre","text":"Block"},{"type":"cl","text":"Checklist Item 1","checked":false},{"type":"cl","text":"Checklist Item 2 (checked)","checked":true}]}}'
 const expectedOutput = ''
 
-describe('convertPost', () => {
+describe('applyFormats', () => {
+  let params
 
+  beforeEach('prepare', () => {
+    params = { 'type': 'p', 'text': 'Code Block', 'formats': { 'code': [0, 10] } }
+  })
+
+  it('should apply format for whole string', () => {
+    const text = applyFormats(params)
+    expect(text).eql('```Code Block```')
+  })
+
+  it('should apply format for part of the string', () => {
+    params.formats.code = [5, 10]
+    const text = applyFormats(params)
+    expect(text).eql('Code ```Block```')
+  })
+
+  it('should apply several formats at the same time', () => {
+    params.formats.code = [5, 10]
+    params.formats.strike = [0, 4]
+    const text = applyFormats(params)
+    expect(text).eql('~~Code~~ ```Block```')
+  })
+})
+
+describe.skip('convertPost', () => {
   it('should convert to the expected output', () => {
     const output = convertPost(testInput)
     console.log(output)
