f12e59d09951826ea56cb30482bdd82d49ff5cf9
Christian Bick <christian.bick@bitsuppliers.com>
2019-06-30 16:06:44 +0200

    Remove accidental double registration

--- src/chat/index.js
+++ src/chat/index.js
@@ -1,4 +1,3 @@
-import { openChat } from '../chat-open/open-chat'
 import { forwardMessage } from './forward-message'
 import { informJoinedUser, rejoinLinkedGroup } from './chat-health'
 import { forwardReaction } from './forward-reaction'
@@ -6,7 +5,6 @@ import { forwardFileUpdate } from './forward-file-update'
 
 export default (app) => {
   app.message(forwardMessage(app))
-  app.action({ callback_id: 'open-chat' }, openChat(app))
   app.event('group_left', rejoinLinkedGroup(app))
   app.event('member_joined_channel', informJoinedUser(app))
   app.event('reaction_added', forwardReaction(app))
