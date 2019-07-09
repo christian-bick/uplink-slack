39ce960f63e48ef820e2f29f42de0df5af8d62eb
Christian Bick <christian.bick@bitsuppliers.com>
2019-07-09 09:50:35 +0200

    Fix chat-health

--- src/chat/chat-health.js
+++ src/chat/chat-health.js
@@ -1,17 +1,17 @@
 import store from '../store'
 
-export const buildJoinUserInfo = (joinedUserId, userProfile, contactProfile) => (
-  `Hi <@${joinedUserId}>! This group is linked to an external conversation with ${contactProfile.name}. ` +
-  `You can follow their conversation here but I will only forward messages of ${userProfile.name}.`
+export const buildInvitingUserInfo = (invitingUserId) => (
+  `<@${invitingUserId}> This group is a direct message conversation. For group conversations, use Slack's built in shared channels.`
 )
 
 export const rejoinLinkedGroup = (app) => async ({ event, context }) => {
   const channelId = event.channel
-  const slackGroup = await store.slack.group.get([context.teamId, channelId])
+  const slackGroup = await store.slack.conversation.get([context.teamId, channelId])
   if (!slackGroup) {
     return
   }
-  const slackUser = await store.slack.user.get([slackGroup.source.teamId, slackGroup.source.userId])
+  const medium = await store.account.medium.get(slackGroup.sourceAccountId)
+  const slackUser = await store.slack.user.get([medium.teamId, medium.userId])
   app.client.conversations.invite({
     token: slackUser.userToken,
     channel: channelId,
@@ -19,18 +19,20 @@ export const rejoinLinkedGroup = (app) => async ({ event, context }) => {
   })
 }
 
-export const informJoinedUser = (app) => async ({ event, context }) => {
-  const channelId = event.channel
-  const slackGroup = await store.slack.group.get([context.teamId, channelId])
-  if (!slackGroup || event.user === slackGroup.source.userId || event.user === context.botId) {
+export const removeJoinedUser = (app) => async ({ event, context }) => {
+  const slackGroup = await store.slack.conversation.get([context.teamId, event.channel])
+  if (!slackGroup || context.accountId === slackGroup.sourceAccountId || event.user === context.botId) {
     return
   }
-  const userProfile = await store.slack.profile.get([slackGroup.source.teamId, slackGroup.source.userId])
-  const contactRegistration = await store.user.registration.get(slackGroup.sink.email)
-  const contactProfile = await store.slack.profile.get([contactRegistration.teamId, contactRegistration.userId])
-  app.client.chat.postMessage({
+  const invitingUser = await store.slack.user.get([context.teamId, event.inviter])
+  await app.client.conversations.kick({
+    token: invitingUser.userToken,
+    channel: event.channel,
+    user: event.user
+  })
+  await app.client.chat.postMessage({
     token: context.botToken,
-    channel: channelId,
-    text: buildJoinUserInfo(event.user, userProfile, contactProfile)
+    channel: event.channel,
+    text: buildInvitingUserInfo(event.inviter)
   })
 }
--- src/chat/index.js
+++ src/chat/index.js
@@ -1,5 +1,5 @@
 import { forwardMessage } from './forward-message'
-import { informJoinedUser, rejoinLinkedGroup } from './chat-health'
+import { removeJoinedUser, rejoinLinkedGroup } from './chat-health'
 import { forwardReaction } from './forward-reaction'
 import { forwardFileUpdate } from './forward-file-update'
 import { catchAsync } from '../errors-async'
@@ -7,7 +7,7 @@ import { catchAsync } from '../errors-async'
 export default (app) => {
   app.message(forwardMessage(app))
   app.event('group_left', catchAsync(rejoinLinkedGroup(app)))
-  app.event('member_joined_channel', catchAsync(informJoinedUser(app)))
+  app.event('member_joined_channel', catchAsync(removeJoinedUser(app)))
   app.event('reaction_added', catchAsync(forwardReaction(app)))
   app.event('reaction_removed', catchAsync(forwardReaction(app, true)))
   app.event('file_change', catchAsync(forwardFileUpdate(app)))
