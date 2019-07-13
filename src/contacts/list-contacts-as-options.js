118c66a2185718027da322e2b0b0043af87c2948
Christian Bick <christian.bick@bitsuppliers.com>
2019-07-13 15:00:10 +0200

    Fix missing decryption when generating contact list

--- src/contacts/list-contacts-as-options.js
+++ src/contacts/list-contacts-as-options.js
@@ -1,11 +1,12 @@
 import store from '../store'
 import redis from '../redis'
 import { accountProfileKey } from '../redis-keys'
+import { decrypt } from '../redis-crypto'
 
 export const listContactsAsOptions = (app) => async ({ body, context, ack }) => {
   const searchString = body.value
   const contacts = await store.account.contacts.smembers(context.accountId)
-  const contactProfiles = await contacts.reduce((multi, accountId) => multi.get(accountProfileKey(accountId)), redis.multi()).execAsync().map(JSON.parse)
+  const contactProfiles = await contacts.reduce((multi, accountId) => multi.get(accountProfileKey(accountId)), redis.multi()).execAsync().map(decrypt).map(JSON.parse)
   const zippedProfiles = contacts.map((accountId, index) => ({ ...contactProfiles[index], accountId }))
 
   const filteredProfiles = zippedProfiles.filter(profile => profile.name.includes(searchString))
