cb9aa6c24c73644c015b8cc4b96f629764939c82
Christian Bick <christian.bick@bitsuppliers.com>
2019-07-15 19:55:37 +0200

    DRY

--- src/redis-keys.js
+++ src/redis-keys.js
@@ -2,18 +2,22 @@ import { hashed } from './redis-crypto'
 
 const key = (prefixes, identifiers) => `${prefixes.join('-')}:${identifiers.join('-')}`
 
+export const accountKey = (suffix, identifiers) => key(['account'].concat(suffix), identifiers)
+export const slackKey = (suffix, identifiers) => key(['slack'].concat(suffix), identifiers)
+export const usageKey = (suffix, identifiers) => key(['usage'].concat(suffix), identifiers)
+
 export const registrationKey = (email) => key(['registration'], [hashed(email)])
 export const invitesKey = (email) => key(['invites'], [hashed(email)])
 
-export const accountMediumKey = (accountId) => key(['account', 'medium'], [ accountId ])
-export const accountProfileKey = (accountId) => key(['account', 'profile'], [ accountId ])
-export const accountContactsKey = (accountId) => key(['account', 'contacts'], [ accountId ])
-export const accountBlacklistKey = (accountId) => key(['account', 'blacklist'], [ accountId ])
-export const accountLinkKey = (sourceAccountId, sinkAccountId) => key(['account', 'link'], [sourceAccountId, sinkAccountId])
+export const accountMediumKey = (accountId) => accountKey('medium', [ accountId ])
+export const accountProfileKey = (accountId) => accountKey('profile', [ accountId ])
+export const accountContactsKey = (accountId) => accountKey('contacts', [ accountId ])
+export const accountBlacklistKey = (accountId) => accountKey('blacklist', [ accountId ])
+export const accountLinkKey = (sourceAccountId, sinkAccountId) => accountKey('link', [sourceAccountId, sinkAccountId])
 
-export const slackUserKey = (teamId, userId) => key(['slack', 'user'], [ teamId, userId ])
-export const slackTeamKey = (teamId) => key(['slack', 'team'], [teamId])
-export const slackConversationKey = (teamId, groupId) => key(['slack', 'conversation'], [ teamId, groupId ])
+export const slackUserKey = (teamId, userId) => slackKey('user', [ teamId, userId ])
+export const slackTeamKey = (teamId) => slackKey('team', [teamId])
+export const slackConversationKey = (teamId, channelId) => slackKey('conversation', [ teamId, channelId ])
 
-export const usageInvitesKey = (accountId) => key(['usage', 'invites'], [ accountId ])
-export const usageChatsKey = (accountId) => key(['usage', 'chats'], [ accountId ])
+export const usageInvitesKey = (accountId) => usageKey('invites', [ accountId ])
+export const usageChatsKey = (accountId) => usageKey('chats', [ accountId ])
