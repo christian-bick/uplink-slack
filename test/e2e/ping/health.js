81cffb21531ef01978a7059eaeb37be04e0906a0
Christian Bick <christian.bick@bitsuppliers.com>
2019-07-20 17:19:14 +0200

    Remove old test infrastructure

--- test/e2e/ping/health.js
+++
@@ -1,9 +0,0 @@
-import { SERVER_ADDRESS } from '../test-server'
-import request from 'co-request'
-
-describe('Health check', () => {
-  it('should return 200 on root', async () => {
-    const resp = await request(`${SERVER_ADDRESS}/`)
-    expect(resp.statusCode).to.equal(200)
-  })
-})
