e99b3bbbac112dd9571ca9181cb5b0a59cf40c18
Christian Bick <christian.bick@bitsuppliers.com>
2019-07-20 17:36:48 +0200

    User docker compose up

--- test/e2e/run.sh
+++ test/e2e/run.sh
@@ -1,12 +1,7 @@
-trap "exit" INT TERM ERR
-trap "kill 0" EXIT
-
 export LOG_LEVEL=debug
 export REDIS_MOCK=true
 export PORT=3002
 
-npm run dev &
-./node_modules/wait-on/bin/wait-on http://localhost:3002
+docker-compose up &
+./node_modules/wait-on/bin/wait-on http://localhost:3000
 ./node_modules/.bin/cypress run
-
-wait
