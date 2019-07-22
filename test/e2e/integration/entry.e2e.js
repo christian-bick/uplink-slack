90a99be761f7bfd3a6f4f27c83edb42e4ed5bb31
Christian Bick <christian.bick@bitsuppliers.com>
2019-07-22 21:53:11 +0200

    Complete blacklist test

--- test/e2e/integration/entry.e2e.js
+++ test/e2e/integration/entry.e2e.js
@@ -1,3 +1,5 @@
+import { blacklisted } from '../fixtures/users'
+
 describe('App Entry', () => {
   beforeEach('open app home', () => {
     cy.visitAppHome()
@@ -11,16 +13,18 @@ describe('App Entry', () => {
   })
 
   describe('manage blacklist', () => {
-
     describe('with blacklist entry', () => {
       beforeEach(() => {
         cy.task('prepareBlacklist')
       })
 
-      it.only('should open manage blacklist dialog', () => {
+      it.only('should find entry in blacklist dialog and remove it', () => {
         cy.get('[data-qa="bk_actions_block_action"] [type="ellipsis"]').last().click()
         cy.get('[data-qa="menu_item_button"]').contains('Manage Blacklist').last().click()
         cy.get('[data-qa="dialog"]')
+        cy.get('[data-qa="select_input"]').click()
+        cy.get('[data-qa^="select_option"]').contains(blacklisted.profile.name).click()
+        cy.get('[data-qa="dialog_go"').click()
       })
     })
   })
